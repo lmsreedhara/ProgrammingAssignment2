6237b84d162076cad2399a883488a9a1b563f29c
lmsreedhara <54691285+lmsreedhara@users.noreply.github.com>
2023-08-05 02:35:52 -0400

    google colab file

---
+++ Test.py
@@ -0,0 +1,32 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyNbgYP4KJespphQ1JSayByh"
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "code",
+      "source": [
+        "#Test\n",
+        "\n",
+        "import pandas as pd"
+      ],
+      "metadata": {
+        "id": "z76mFDfnYP9d"
+      },
+      "execution_count": null,
+      "outputs": []
+    }
+  ]
+}
